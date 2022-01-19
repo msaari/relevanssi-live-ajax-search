6dfbeeb9df695b875c63ba44cfc08b17759110c0
Mikko Saari <mikko@mikkosaari.fi>
2022-01-19 10:55:32 +0200

    Renaming the template

--- templates/search-results.php
+++ templates/search-results.php
@@ -1,35 +1,37 @@
 <?php
 /**
- * Search results are contained within a div.searchwp-live-search-results
+ * Search results are contained within a div.relevanssi-live-search-results
  * which you can style accordingly as you would any other element on your site
  *
  * Some base styles are output in wp_footer that do nothing but position the
  * results container and apply a default transition, you can disable that by
  * adding the following to your theme's functions.php:
  *
- * add_filter( 'searchwp_live_search_base_styles', '__return_false' );
+ * add_filter( 'relevanssi_live_search_base_styles', '__return_false' );
  *
  * There is a separate stylesheet that is also enqueued that applies the default
  * results theme (the visual styles) but you can disable that too by adding
  * the following to your theme's functions.php:
  *
- * wp_dequeue_style( 'searchwp-live-search' );
+ * wp_dequeue_style( 'relevanssi-live-search' );
  *
- * You can use ~/searchwp-live-search/assets/styles/style.css as a guide to customize
+ * You can use ~/relevanssi-live-search/assets/styles/style.css as a guide to customize
  */
 ?>
 
 <?php if ( have_posts() ) : ?>
-	<?php while ( have_posts() ) : the_post(); ?>
-		<?php $post_type = get_post_type_object( get_post_type() ); ?>
-		<div class="searchwp-live-search-result" role="option" id="" aria-selected="false">
+	<?php
+	while ( have_posts() ) :
+		the_post();
+	?>
+		<div class="relevanssi-live-search-result" role="option" id="" aria-selected="false">
 			<p><a href="<?php echo esc_url( get_permalink() ); ?>">
 				<?php the_title(); ?> &raquo;
 			</a></p>
 		</div>
 	<?php endwhile; ?>
 <?php else : ?>
-	<p class="searchwp-live-search-no-results" role="option">
-		<em><?php esc_html_e( 'No results found.', 'searchwp-live-ajax-search' ); ?></em>
+	<p class="relevanssi-live-search-no-results" role="option">
+		<em><?php esc_html_e( 'No results found.', 'relevanssi-live-ajax-search' ); ?></em>
 	</p>
 <?php endif; ?>
